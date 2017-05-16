2b8ef486127b69e4218fb4909025ce542c0cb3c5
pliska <liskape8@gmail.com>
2017-05-16 16:50:33 +0200

    Use API v11

--- lib/bing-ads-api/service/customer_management.rb
+++ lib/bing-ads-api/service/customer_management.rb
@@ -1,13 +1,13 @@
 # -*- encoding : utf-8 -*-
 module BingAdsApi
-	
-	
-	# Public : This class represents the Customer Management Services 
-	# defined in the Bing Ads API, to manage customer accounts 
-	# 
-	# Author:: jlopezn@neonline.cl 
-	# 
-	# Examples 
+
+
+	# Public : This class represents the Customer Management Services
+	# defined in the Bing Ads API, to manage customer accounts
+	#
+	# Author:: jlopezn@neonline.cl
+	#
+	# Examples
 	#  options = {
 	#    :environment => :sandbox,
 	#    :username => "username",
@@ -19,13 +19,13 @@ module BingAdsApi
 	class CustomerManagement < BingAdsApi::Service
 
 
-		# Public : Constructor 
-		# 
-		# Author:: jlopezn@neonline.cl 
-		# 
+		# Public : Constructor
+		#
+		# Author:: jlopezn@neonline.cl
+		#
 		# options - Hash with the parameters for the client proxy and the environment
-		# 
-		# Examples 
+		#
+		# Examples
 		#   options = {
 		#     :environment => :sandbox,
 		#     :username => "username",
@@ -44,26 +44,26 @@ module BingAdsApi
 		## Operations Wrappers ##
 		#########################
 
-		# Public : Gets a list of objects that contains account identification information, 
+		# Public : Gets a list of objects that contains account identification information,
 		# for example the name and identifier of the account, for the specified customer.
-		# 
-		# Author:: jlopezn@neonline.cl 
-		# 
+		#
+		# Author:: jlopezn@neonline.cl
+		#
 		# === Parameters
 		# +customer_id+ - identifier for the customer who owns the accounts. If nil, then the authentication customer id is used
-		# +only_parent_accounts+ - boolean to determine whether to return only the accounts that belong to the customer or to also 
-		# return the accounts that the customer manages for other customers. Default false 
-		# 
-		# === Examples 
-		#   customer_management_service.get_accounts_info 
+		# +only_parent_accounts+ - boolean to determine whether to return only the accounts that belong to the customer or to also
+		# return the accounts that the customer manages for other customers. Default false
+		#
+		# === Examples
+		#   customer_management_service.get_accounts_info
 		#   # => Array[BingAdsApi::AccountsInfo]
-		# 
+		#
 		# Returns:: Array of BingAdsApi::AccountsInfo
-		# 
+		#
 		# Raises:: exception
 		def get_accounts_info(customer_id=nil, only_parent_accounts=false)
-			response = call(:get_accounts_info, 
-				{customer_id: customer_id || self.client_proxy.customer_id, 
+			response = call(:get_accounts_info,
+				{customer_id: customer_id || self.client_proxy.customer_id,
 				only_parent_accounts: only_parent_accounts.to_s})
 			response_hash = get_response_hash(response, __method__)
 			accounts = response_hash[:accounts_info][:account_info].map do |account_hash|
@@ -72,12 +72,20 @@ module BingAdsApi
 			return accounts
 		end
 
+		def get_accounts_info
+			call(:get_accounts_info, {})
+		end
+
+		def get_customers_info
+			call(:get_customers_info, { application_scope: "Advertiser", customer_name_filetr: "", top_n: 100 })
+			# error: The Customer.Name property must be between 1 and 100 characters long.
+		end
 
 		private
 			def get_service_name
 				"customer_management"
 			end
-			
+
 	end
 
 end
